67f450b9cfa7f827455f6855ddc39228ae356d30
Kerwin <kerwinmondesir@outlook.com>
2024-07-29 00:39:55 -0400

    Updated Bookings class with bookingrefno. and bookingdatetime!

--- Bookings.java
+++ Bookings.java
@@ -1,2 +1,24 @@
+import java.time.LocalDateTime;
+import java.util.UUID;
 public class Bookings {
+    private final UUID bookingRefNo;
+    private final LocalDateTime bookingDateTime;
+
+    public Bookings(){
+        this.bookingRefNo = UUID.randomUUID();
+        this.bookingDateTime = LocalDateTime.now();
+    }
+    //Getters
+    public UUID getBookingRefNo() {
+        return bookingRefNo;
+    }
+    public LocalDateTime getBookingDateTime(){
+        return bookingDateTime;
+    }
+    @Override
+    public String toString(){
+        return "Booking Reference no. " + bookingRefNo + " \n" +
+                "Booking Date & Time: " + bookingDateTime;
+
+    }
 }
