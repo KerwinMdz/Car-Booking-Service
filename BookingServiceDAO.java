08883c36e2bee6fd993630bdf47251400ead035d
Kerwin <kerwinmondesir@outlook.com>
2025-09-15 23:25:53 -0300

    fixed minor issue

--- BookingServiceDAO.java
+++ BookingServiceDAO.java
@@ -1,18 +1,18 @@
 import java.util.ArrayList;
 
 public class BookingServiceDAO {
-    private final ArrayList<User> users;
-    private final ArrayList<Bookings> bookings;
+    private static ArrayList<User> users;
+    private static ArrayList<Bookings> bookings;
 
     public BookingServiceDAO(ArrayList<User> user, ArrayList<Bookings> booking){
         this.users = user;
         this.bookings = booking;
     }
-    public ArrayList<User> getAllUsers(){
+    public static ArrayList<User> getAllUsers(){
         return users;
     }
 
-    public ArrayList<Bookings> getBookings() {
+    public static ArrayList<Bookings> getBookings() {
         return bookings;
     }
 
