bb0774f2c6e458df063ee52bb96a826279161a01
Kerwin <kerwinmondesir@outlook.com>
2025-09-15 23:07:11 -0300

    Added BookingService class to store and retrieve bookings/users

---
+++ BookingServiceDAO.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class BookingServiceDAO {
+    //private ArrayList<Car> cars;
+    private final ArrayList<User> users;
+    private final ArrayList<Bookings> bookings;
+
+    public BookingServiceDAO(ArrayList<User> user, ArrayList<Bookings> booking){
+        this.users = user;
+        this.bookings = booking;
+    }
+    public ArrayList<User> getAllUsers(){
+        return users;
+    }
+
+    public ArrayList<Bookings> getBookings() {
+        return bookings;
+    }
+
+    public void addUser(User user){
+        users.add(user);
+    }
+
+    public void addBooking(Bookings booking){
+        bookings.add(booking);
+    }
+}
+
